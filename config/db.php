13e43b9848c2ae5db8644c168d952ba197bb8650
sarawoot40 <sarawoot40@hotmail.com>
2018-09-20 10:57:44 +0700

    up db

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=rdsurvey_admin',
-    'username' => '<username of DB>',
-    'password' => '<password of DB>',
+    'username' => 'root',
+    'password' => '',
     // 'dsn' => 'mysql:host=localhost;dbname=mapmaung_taxmap',
     // 'username' => 'mapmaung_taxmap',
     // 'password' => 'b9W6m0l7Xq',
